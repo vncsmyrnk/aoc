788b200ec78381aa207aa935b7753cbc636a687d
Vinicius Mayrink <vmayrink12@gmail.com>
2025-02-02 17:04:07 -0300

    feat(2024/day1): adds second part resolution

---
+++ 2024/day1/second_part.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Solves the second part of day 1.
+# Takes the same input, multiplies the first column elements by
+# the amount of times they appear on the second column
+
+if [ ! -f ./input.txt ]; then
+  echo "input file required. Check project or AOC docs for more information about how to to get it"
+  exit 1
+fi
+
+awk '{ print $1 }' input.txt > /tmp/first-column.txt
+awk '{ print $2 }' input.txt > /tmp/second-column.txt
+awk '{ print $1 }' input.txt | xargs -I {} grep -c {} /tmp/second-column.txt > /tmp/first-to-second-similarity-weight.txt
+paste /tmp/first-column.txt /tmp/first-to-second-similarity-weight.txt > /tmp/first-column-similarity-weights.txt
+awk '{ sum += $1 * $2 }; END { print sum }' /tmp/first-column-similarity-weights.txt
